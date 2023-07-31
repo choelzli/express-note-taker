060da5d0f5115a2f99956e07c9ec38519e67acee
choelzli <128326139+choelzli@users.noreply.github.com>
2023-07-30 23:58:11 -0400

    more troubleshooting

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/notes', api);
 
-app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-    );
+app.get('/', (req, res) => {
+    res.sendFile('/public/index.html')
+});
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
-})
+});
 
 app.listen(PORT, () => 
     console.log(`Express server listening on Port ${PORT}.`)
